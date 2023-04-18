009c6fb8c15c4287878116b6db5ca4431d0d5864
TokiLoshi <103384402+TokiLoshi@users.noreply.github.com>
2023-04-18 15:51:37 -0700

    Program works, keeping the detailed console logging for now

--- app.js
+++ app.js
@@ -1,31 +1,32 @@
-console.log("Hello World")
+console.log("Welcome to rock paper scissors")
 
 // Get computer choice 
 function getComputerChoice() {
   const options = ['rock', 'paper', 'scissors']
   const compChoice = Math.floor(Math.random() * 3)
-  console.log(`Should be random: ${compChoice}`)
-  console.log(`Computer's choice: ${options[compChoice]}`)
   // Randomly return 'Rock', 'Paper' or 'Scissors'
   return options[compChoice]
 }
 
 // Get player choice: 
 function playerSelection() {
+  
   // prompt player for selection
   const playerChoice = prompt("Rock, Paper or Scissors?")
-  console.log(`Player's choice: ${playerChoice}`)
+  
   // Should be case insensitive  
   const lowerChoice = playerChoice.toLowerCase()
-  console.log(`Choice to lower: ${lowerChoice}`)
+  return lowerChoice 
+
+  // To DO: Catch invalid
   // Check it's one of the following or return invalid
-  if (lowerChoice !== 'rock' && lowerChoice !== 'paper' && lowerChoice !== 'scissors') {
-    console.log(`We have an invalid choice: ${lowerChoice}`)
-  }
-  else {
-    console.log(`Player selection: ${lowerChoice} is valid`)
-    return lowerChoice
-  }
+  // if (lowerChoice !== 'rock' && lowerChoice !== 'paper' && lowerChoice !== 'scissors') {
+  //   console.log(`We have an invalid choice: ${lowerChoice}`)
+  // }
+  // else {
+  //   console.log(`Player selection: ${lowerChoice} is valid`)
+  //   return lowerChoice
+  // }
 
 }
 
@@ -33,67 +34,98 @@ function playerSelection() {
 function playRound(playerSelection, getComputerChoice) {
   const player = playerSelection
   const computer = getComputerChoice
-  console.log(`In round player: ${player}`)
-  console.log(`In round computer: ${computer}`)
-
+  
   // It's a tie if computer and player choice the same
   let winner = null;
   if (player === computer) {
     console.log('It\'s a tie')
-    winner = 'TIE'
+    winner = 'Tie'
   }
 
   // Paper beats rock 
   else if (computer === 'rock'){
     if (player === 'paper'){
-      console.log(`Player wins, they chose: ${player} and computer chose: ${computer}`)
-      winner = 'You won!'
+      winner = 'player'
+      console.log(`You win ${player} beats chose: ${computer}`)
     }
     else {
-      winner = 'You lost'
-      console.log(`Player loses, they chose: ${player} and computer chose: ${computer}`)
+      winner = 'computer'
+      console.log(`You lost ${computer} beats ${player}`)
     }
   }
 
   // Scissors beat paper 
   else if (computer === 'paper'){
     if (player === 'scissors'){
-      winner = 'You won'
-      console.log(`Player wins, they chose ${player} and computer chose: ${computer}`)
+      winner = 'player'
+      console.log(`You win ${player} beats ${computer}`)
     }
     else {
-      winner = 'You lost'
-      console.log(`Player lost they chose ${player}, computer chose: ${computer}`)
+      winner = 'computer'
+      console.log(`You lost ${computer} beats ${player}`)
     }
   }
 
   // Rock beatus scissors 
   else{
     if (player === 'rock'){
-      winner = 'You won' 
-      console.log(`Player wins they chose: ${player}, computer chose: ${computer}`)
+      winner = 'player'
+      console.log(`You won ${player} beats ${computer}`)
     }
     else {
-      winner = 'You lost'
-      console.log(`Player lost they chose: ${player}, computer chose: ${computer}`)
+      winner = 'computer'
+      console.log(`You lost ${computer} beats ${computer}`)
     }
   }
-
-  console.log(`We have a decision: ${winner}`)
+  console.log(`Winner is: ${winner}`)
+  return winner
 
 }
-const playerChoice = playerSelection()
-const computerChoice = getComputerChoice()
-console.log(`This should be player's choice: ${playerChoice}`)
-console.log(`This should be computer's choice: ${computerChoice}`)
-playRound(playerChoice, computerChoice)
 
-
-// function takes Player's selection and computer's selection
-// Declares winner with something like 'You Lose! Paper beatss Rock' 
-
-// Function called game that has playRound inside of it 
 // Play a 5 round game that keeps the score
-// Report a winner or loser at the end 
-// Make sure to be returning from all functions
+function game() {
+  // Set scores to 0
+  let computerScore = 0
+  let playerScore = 0
+
+  for (i = 1; i <= 5; i++){
+    console.log(`Round: ${i}`)
+    console.log(`Score so far: player has ${playerScore} points and computer has ${computerScore} points`)
+    const playerChoice = playerSelection()
+    const computerChoice = getComputerChoice()
+
+    // Determine the round winner
+    let roundWinner = playRound(playerChoice, computerChoice)
+    console.log(`Round winner ${i} is ${roundWinner}`)
+    // Increment scores 
+    if (roundWinner === 'Tie'){
+      computerScore += 0
+      playerScore += 0
+    }
+    else if (roundWinner === 'player'){
+      playerScore += 1
+    }
+    else{
+      computerScore +=1
+    }
+    console.log(`End of round, here are the scores the player has ${playerScore} and the computer has: ${computerScore} points`)
+  }
+
+  // Report a winner or loser at the end 
+  console.log(`End of game: Player got ${playerScore} computer got ${computerScore}`)
+  if (computerScore === playerScore){
+    console.log(`It\'s a tie, you and the computer both got ${computerScore} and ${playerScore} points`)
+  }
+  else if (computerScore >= playerScore){
+    console.log(`Computer won they got ${computerScore} and you got ${playerScore}`)
+  }
+  else{
+    console.log(`You won! You got ${playerScore} points and the computer got ${computerScore} points`)
+  }
+}
+
+game()
+
+
+
 
