38e05b89e7611dfc781047d27b9ee1c336e53f0e
TokiLoshi <103384402+TokiLoshi@users.noreply.github.com>
2023-04-26 19:19:17 -0700

    Reworking project with UI

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ console.log("Welcome to rock paper scissors")
 
 // Get computer choice 
 function getComputerChoice() {
+  console.log("Just a moment, the computer is choosing")
   const options = ['rock', 'paper', 'scissors']
   const compChoice = Math.floor(Math.random() * 3)
   // Randomly return 'Rock', 'Paper' or 'Scissors'
@@ -10,23 +11,29 @@ function getComputerChoice() {
 
 // Get player choice: 
 function playerSelection() {
+  console.log('Your turn');
+  const yourTurn = document.createElement('p');
+  console.log(`Your turn: ${yourTurn}`)
+  const turn = document.querySelector('.container');
+  console.log(`turn: ${turn}`)
+  yourTurn.textContent = "Your turn";
+  turn.append(yourTurn);
+  const buttons = document.querySelectorAll('button');
+
+  buttons.forEach(button => {
+    button.addEventListener('click', () => {
+      alert(button.id);
+      const playerChoice = button.id;
+      console.log(`Player's choice: ${playerChoice}`);
+      return playerChoice;
+    })
+  })
   
   // prompt player for selection
-  const playerChoice = prompt("Rock, Paper or Scissors?")
-  
+  // const playerChoice = prompt("Rock, Paper or Scissors?")
   // Should be case insensitive  
-  const lowerChoice = playerChoice.toLowerCase()
-  return lowerChoice 
-
-  // To DO: Catch invalid
-  // Check it's one of the following or return invalid
-  // if (lowerChoice !== 'rock' && lowerChoice !== 'paper' && lowerChoice !== 'scissors') {
-  //   console.log(`We have an invalid choice: ${lowerChoice}`)
-  // }
-  // else {
-  //   console.log(`Player selection: ${lowerChoice} is valid`)
-  //   return lowerChoice
-  // }
+  // const lowerChoice = playerChoice.toLowerCase()
+  // return lowerChoice 
 
 }
 
@@ -87,41 +94,51 @@ function game() {
   // Set scores to 0
   let computerScore = 0
   let playerScore = 0
+  let round = 0
 
-  for (i = 1; i <= 5; i++){
-    console.log(`Round: ${i}`)
-    console.log(`Score so far: player has ${playerScore} points and computer has ${computerScore} points`)
-    const playerChoice = playerSelection()
-    const computerChoice = getComputerChoice()
+  const computerChoice = getComputerChoice();
+  console.log(computerChoice);
+  const playerChoice = playerSelection();
+  console.log(playerChoice);
+  let roundWinner = playRound(playerChoice, computerChoice);
+  console.log(`Round winner in game: ${roundWinner}`);
+  
+  // This is the part that plays exactly 5 rounds
+  // for (i = 1; i <= 5; i++){
+  //   console.log(`Round: ${i}`)
+  //   console.log(`Score so far: player has ${playerScore} points and computer has ${computerScore} points`)
+  //   const playerChoice = playerSelection()
+  //   const computerChoice = getComputerChoice()
 
     // Determine the round winner
-    let roundWinner = playRound(playerChoice, computerChoice)
-    console.log(`Round winner ${i} is ${roundWinner}`)
-    // Increment scores 
-    if (roundWinner === 'Tie'){
-      computerScore += 0
-      playerScore += 0
-    }
-    else if (roundWinner === 'player'){
-      playerScore += 1
-    }
-    else{
-      computerScore +=1
-    }
-    console.log(`End of round, here are the scores the player has ${playerScore} and the computer has: ${computerScore} points`)
-  }
+  //   let roundWinner = playRound(playerChoice, computerChoice)
+  //   console.log(`Round winner ${i} is ${roundWinner}`)
+  //   // Increment scores 
+  //   if (roundWinner === 'Tie'){
+  //     computerScore += 0
+  //     playerScore += 0
+  //   }
+  //   else if (roundWinner === 'player'){
+  //     playerScore += 1
+  //   }
+  //   else{
+  //     computerScore +=1
+  //   }
+  //   console.log(`End of round, here are the scores the player has ${playerScore} and the computer has: ${computerScore} points`)
+  // }
 
   // Report a winner or loser at the end 
-  console.log(`End of game: Player got ${playerScore} computer got ${computerScore}`)
-  if (computerScore === playerScore){
-    console.log(`It\'s a tie, you and the computer both got ${computerScore} and ${playerScore} points`)
-  }
-  else if (computerScore >= playerScore){
-    console.log(`Computer won they got ${computerScore} and you got ${playerScore}`)
-  }
-  else{
-    console.log(`You won! You got ${playerScore} points and the computer got ${computerScore} points`)
-  }
+  // console.log(`End of game: Player got ${playerScore} computer got ${computerScore}`)
+  // if (computerScore === playerScore){
+  //   console.log(`It\'s a tie, you and the computer both got ${computerScore} and ${playerScore} points`)
+  // }
+  // else if (computerScore >= playerScore){
+  //   console.log(`Computer won they got ${computerScore} and you got ${playerScore}`)
+  // }
+  // else{
+  //   console.log(`You won! You got ${playerScore} points and the computer got ${computerScore} points`)
+  // }
+  console.log("End of game")
 }
 
 game()
